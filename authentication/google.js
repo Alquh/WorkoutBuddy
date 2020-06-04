44b2360a3444db5816d1355e66093f2734294c9e
Matt Gilbert <mattgilbert90@hotmail.com>
2020-06-03 20:17:46 -0400

    Revert "latest changes"
    This reverts commit a7db39312b49260893cf9b3935b045e09187cedc.

--- authentication/google.js
+++ authentication/google.js
@@ -1,63 +1,32 @@
-import { React, Component } from 'react';
-import { Text } from 'react-native';
-import * as GoogleSignIn from 'expo-google-sign-in';
+import * as Google from "expo-google-app-auth";
+
+// important to note that this package has been deprecated.
 import { androidClientId, iosClientId } from "../superSecretKey";
 
+async function googleAuthentication() {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const result = await Google.logInAsync({
+				behavior: "web",
+				androidClientId: androidClientId,
+				iosClientId: iosClientId,
+				scopes: ["profile", "email"],
+			});
 
-class Google extends Component {
+			// Want to see your result? Uncomment the following lines.
+			// console.log("Result");
+			// console.log(result);
 
-  state = { user: null };
-
-  componentDidMount() {
-    this.initAsync();
-  }
-
-  initAsync = async () => {
-	  try {
-    await GoogleSignIn.initAsync({
-      // You may ommit the clientId when the firebase `googleServicesFile` is configured
-	  iosClientId: iosClientId,
-	  androidClientId: androidClientId,
-	  //scopes: ["profile", "email"],
+			// Always return a consistend object model.
+			if (result.type === "success") {
+				resolve({ result: result.accessToken, success: true });
+			} else {
+				resolve({ result: "cancelled", success: false });
+			}
+		} catch (e) {
+			resolve({ result: "error", success: false });
+		}
 	});
-	} catch ({ message }) {
-  alert('GoogleSignIn.initAsync(): ' + message);
 }
-    this._syncUserWithStateAsync();
-  };
 
-  _syncUserWithStateAsync = async () => {
-    const user = await GoogleSignIn.signInSilentlyAsync();
-    this.setState({ user });
-  };
-
-  signOutAsync = async () => {
-    await GoogleSignIn.signOutAsync();
-    this.setState({ user: null });
-  };
-
-  signInAsync = async () => {
-    try {
-      await GoogleSignIn.askForPlayServicesAsync();
-      const { type, user } = await GoogleSignIn.signInAsync();
-      if (type === 'success') {
-        this._syncUserWithStateAsync();
-      }
-    } catch ({ message }) {
-      alert('login: Error:' + message);
-    }
-  };
-
-  onPress = () => {
-    if (this.state.user) {
-      this.signOutAsync();
-    } else {
-      this.signInAsync();
-    }
-  };
-
-  render() {
-    return <Text onPress={this.onPress}>Toggle Auth</Text>;
-  }
-}
-export default Google;
+export default googleAuthentication;
